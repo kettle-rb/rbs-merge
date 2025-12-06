7ae936a6ae844aee513264eecc39215eed53c313
Peter H. Boling <peter.boling@gmail.com>
2025-12-05 19:46:08 -0700

    🎉 Tada!

---
+++ spec/rbs/merge_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rbs::Merge do
+  it "has a version number" do
+    expect(Rbs::Merge::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
