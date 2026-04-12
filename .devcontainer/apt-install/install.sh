06b2a0218e6007b4891b941e938bc4631401f818
Peter H. Boling <peter.boling@gmail.com>
2026-04-12 15:29:47 -0600

    🎨 template cleanup

--- .devcontainer/apt-install/install.sh
+++ .devcontainer/apt-install/install.sh
@@ -3,8 +3,6 @@ set -e  # Exit on error
 
 # Install basic development dependencies for Ruby & JRuby projects
 apt-get update -y
-
-# Basic, commonly needed, dependencies of Ruby & JRuby projects
 apt-get install -y direnv default-jdk git zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
 
 # Support for PostgreSQL (commented out by default)
